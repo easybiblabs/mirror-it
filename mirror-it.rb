826db978a087da203a79136ddfd1de127b436910
Florian Holzhauer <fh-github@fholzhauer.de>
2014-11-17 11:26:08 +0100

    Add debug flag to silence default run, code cleanup

--- mirror-it.rb
+++ mirror-it.rb
@@ -4,6 +4,8 @@ require 'expect'
 require 'English'
 require 'date'
 
+debug = false
+
 repositories = [
   {
     'name' => 'easybib-php55',
@@ -59,44 +61,82 @@ ymd = DateTime.now.strftime('%F')
 repos = ''
 
 repositories.each do |repo|
-  system "aptly mirror show #{repo['name']}"
-  unless $CHILD_STATUS.exitstatus == 0
+  output = `aptly mirror show #{repo['name']}`
+  puts output if debug
+
+  if $CHILD_STATUS.exitstatus == 0
+    puts "mirror #{repo['name']} already exists"
+  else
     # no such mirror
     # import gpg key
     if repo.key?('key')
-     system "gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys #{repo['key']}"
+      output = `gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys #{repo['key']}`
+      puts output if debug
     end
     # create mirror
     if repo.key?('ppa')
-      system "aptly -architectures=\"amd64,i386\" mirror create #{repo['name']} #{repo['ppa']}"
+      output = `aptly -architectures=\"amd64,i386\" mirror create #{repo['name']} #{repo['ppa']}`
+      puts output if debug
     else
-      system "aptly -architectures=\"amd64,i386\" mirror create #{repo['name']} #{repo['archive']} #{repo['dist']}"
+      output = `aptly -architectures=\"amd64,i386\" mirror create #{repo['name']} #{repo['archive']} #{repo['dist']}`
+      puts output if debug
     end
   end
 
-  system "aptly mirror update #{repo['name']}"
-  snapshot="snap-#{repo['name']}-#{ymd}"
-  
-  system "aptly snapshot create #{snapshot} from mirror #{repo['name']}"
+  output = `aptly mirror update #{repo['name']}`
+  puts output if debug
+
+  snapshot = "snap-#{repo['name']}-#{ymd}"
+
+  output = `aptly snapshot show #{snapshot} >/dev/null`
+  puts output if debug
+
+  if $CHILD_STATUS.exitstatus == 0
+    puts "snapshot #{snapshot} already exists, skipping"
+  else
+    output = `aptly snapshot create #{snapshot} from mirror #{repo['name']}`
+    puts output if debug
+  end
   repos += ' ' + snapshot
 end
 
-system "aptly snapshot merge packages-#{ymd} #{repos}"
+output = `aptly snapshot show packages-#{ymd}`
+puts output if debug
 
-system "aptly publish list |grep #{mirror_name}"
 if $CHILD_STATUS.exitstatus == 0
-  # published snapshot exists, just update
-  system "aptly publish switch -passphrase='#{ENV['SIGNING_PASS']}' trusty #{ENV['S3_APT_MIRROR']}  packages-#{ymd}"
+  puts "merged snapshot packages-#{ymd} already exists"
 else
-  system "aptly publish snapshot -passphrase='#{ENV['SIGNING_PASS']}' -distribution='trusty' packages-#{ymd} #{ENV['S3_APT_MIRROR']}"
+  output = `aptly snapshot merge packages-#{ymd} #{repos}`
+  puts output if debug
+
 end
 
-#repositories.each do |repo|
-#  snapshot="snap-#{repo['name']}-#{ymd}"
-#  system "aptly snapshot drop -force #{snapshot}"
-#end
-# FIXME ERROR: unable to drop: snapshot is published
-# We should drop all but current
-#system "aptly snapshot drop -force packages-#{ymd}"
+output = `aptly publish list |grep #{mirror_name}`
+puts output if debug
 
-system "aptly db cleanup"
+if $CHILD_STATUS.exitstatus == 0
+  puts 'switching merged snapshot to todays packages'
+  # published snapshot exists, just update
+  output = `aptly publish switch -passphrase='#{ENV['SIGNING_PASS']}' trusty #{ENV['S3_APT_MIRROR']}  packages-#{ymd}`
+  puts output if debug
+
+else
+  puts 'publishing snapshot'
+  output = `aptly publish snapshot -passphrase='#{ENV['SIGNING_PASS']}' -distribution='trusty' packages-#{ymd} #{ENV['S3_APT_MIRROR']}`
+  puts output if debug
+
+end
+
+# repositories.each do |repo|
+#  snapshot="snap-#{repo['name']}-#{ymd}"
+#  output = `aptly snapshot drop -force #{snapshot}`
+puts output if debug
+
+# end
+# FIXME: ERROR: unable to drop: snapshot is published
+# We should drop all but current
+# output = `aptly snapshot drop -force packages-#{ymd}`
+puts output if debug
+
+output = `aptly db cleanup`
+puts output if debug
